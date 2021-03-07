46ef0d0f1111b9d169645c9e9e39d405e2c42e3d
diegoguerreroborda <diego.guerrero02@uptc.edu.co>
2021-03-06 19:24:04 -0500

    sd

--- indexMS.js
+++ indexMS.js
@@ -2,6 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const shell = require('shelljs');
+const mongoose = require('mongoose');
 const port = 4321;
 
 const app = express()
@@ -12,6 +13,38 @@ function aleatorio() {
 	console.log(number);
     return number;
 }
+
+mongoose.connect('mongodb://localhost/primera_pagina', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+});
+
+//Definir el esquema
+var studentSchema = {
+    id:Number,
+    lastname:String,
+    surname:String,
+    phone:String
+};
+
+var Student = mongoose.model("Student", studentSchema);
+
+var dataC = {
+    id: 1221,
+    lastname: "Buitrago",
+    surname: "Diego",
+    phone: "gfw232"
+};
+
+app.get('/new_student', (req, res) => {
+    //Todavia no sé cómo llega
+    var student = new Student(dataC);
+    student.save(function(err) {
+        console.log(student);
+    });
+})
 
 app.get('/', function (req, res) {
     setTimeout(function(){
